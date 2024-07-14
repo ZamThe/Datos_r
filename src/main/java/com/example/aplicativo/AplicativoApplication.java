be95bf914aef64e5b6c5b2c35a87fc9ad75d3485
ZamThe <jhoan.zamudio@est.iudigital.edu.co>
2024-07-14 11:05:48 -0500

    Corrige detalles

--- src/main/java/com/example/aplicativo/AplicativoApplication.java
+++ src/main/java/com/example/aplicativo/AplicativoApplication.java
@@ -2,11 +2,30 @@ package com.example.aplicativo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AplicativoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AplicativoApplication.class, args);
+    }
+
+    @EnableWebMvc
+    public static class MyConfiguration {
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
     }
 }
